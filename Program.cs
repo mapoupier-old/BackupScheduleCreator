d259941c6724760d52b83ebc9bd2746d07ea80e5
mapoupier <marcandre@poupier.net>
2014-07-08 19:34:50 -0400

    Initial

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackupScheduleCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tapeName = "";
+
+            tapeName = ReadConsole("Please enter tape name: ");
+
+            
+
+            Console.Read();
+        }
+
+
+        static string ReadConsole(string Prompt)
+        {
+            Console.Write(Prompt);
+            return Console.ReadLine();
+        }
+    }
+}
